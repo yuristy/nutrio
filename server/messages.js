2ada1bb405d1267f69d4de11a887d240367a0cc1
Юрий Стрельченя <y.strelchenya@onpoint.ru>
2021-12-05 16:43:31 +0300

    Add message errors

---
+++ server/messages.js
@@ -0,0 +1,8 @@
+const messages = {
+  regError: "Registration error",
+  loginError: "Login error",
+  regSuccess: "Registration is successful",
+  loginSuccess: "Login is successful",
+};
+
+module.exports = messages;
