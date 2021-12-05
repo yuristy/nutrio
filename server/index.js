bdd8fabcd651c2bc16ba41fd3a0b45e4f9e93c13
Юрий Стрельченя <y.strelchenya@onpoint.ru>
2021-12-05 16:43:46 +0300

    Add core logic

--- server/index.js
+++ server/index.js
@@ -1,24 +1,28 @@
-const express = require('express');
+const express = require("express");
 const mongoose = require("mongoose");
-const dotenv = require('dotenv').config();
+const dotenv = require("dotenv").config();
+// const { startServer } = require("./utils");
+const authRouter = require("./routers/authRouter");
 
 const port = process.env.PORT || 5000;
 const login = process.env.LOGIN;
 const password = process.env.PASSWORD;
 
-const dbPath = `mongodb+srv://${login}:${password}@cluster0.fhdhv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const dbPath = `mongodb+srv://${login}:${password}@cluster0.fhdhv.mongodb.net/nutrio?retryWrites=true&w=majority`;
 
 const app = express();
+app.use(express.json());
+app.use("/auth", authRouter);
 
-
-const start = async () => {
+async function startServer() {
   try {
-    await mongoose.connect(dbPath) 
-    app.listen(port, () => {console.log(`Server started on port ${port}`)});
+    await mongoose.connect(dbPath);
+    app.listen(port, () => {
+      console.log(`Server started on port ${port}`);
+    });
   } catch (e) {
     console.log(e);
   }
 }
 
-
-start();
+startServer();
