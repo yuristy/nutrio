d8122df4ac5c051c64a417cfc187873b1ae2aab5
Alex Filenko <a.filenko@onpoint.ru>
2022-01-15 13:05:55 +0300

    Add cookie-parser, cors middlewares. Move db path to dotenv config.

--- server/index.js
+++ server/index.js
@@ -1,17 +1,18 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv").config();
+const cookieParser = require("cookieparser");
+const cors = require("cors");
 const { startServer } = require("./utils");
 const authRouter = require("./routers/authRouter");
 
 const port = process.env.PORT || 5000;
-const login = process.env.LOGIN;
-const password = process.env.PASSWORD;
-
-const dbPath = `mongodb+srv://${login}:${password}@cluster0.fhdhv.mongodb.net/nutrio?retryWrites=true&w=majority`;
+const dbPath = process.env.DBPATH;
 
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
 app.use("/auth", authRouter);
 
 startServer(dbPath, mongoose, app, port);
