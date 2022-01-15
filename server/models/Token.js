cd47f099e5e52031c37d7be1be64efadf4ba2592
Alex Filenko <a.filenko@onpoint.ru>
2022-01-15 13:16:01 +0300

    Add Token model.

---
+++ server/models/Token.js
@@ -0,0 +1,6 @@
+const { Schema, model } = require("mongoose");
+
+const TokenSchema = new Schema({
+  user: { type: Schema.Types.ObjectId, ref: "User" },
+  refreshToken: { type: String, required: true },
+});
