d861a852a742fa3bbe98785524e6d3692e45be59
Alex Filenko <a.filenko@onpoint.ru>
2022-01-15 13:09:10 +0300

    Add isActivated, activationLink fields.

--- server/models/User.js
+++ server/models/User.js
@@ -3,6 +3,8 @@ const { Schema, model } = require("mongoose");
 const User = new Schema({
   username: { type: String, unique: true, required: true },
   password: { type: String, required: true },
+  isActivated: { type: Boolean, default: false },
+  activationLink: { type: String, required: true },
   roles: [{ type: String, refs: "Role" }],
 });
 
