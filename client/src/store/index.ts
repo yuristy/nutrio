61d35559ce4f95110623ee08bcdfad3a8f99db11
Yuri Strelchenya <yoosambee@gmail.com>
2022-03-27 22:29:21 +0300

    Add typescript hooks

--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -14,3 +14,5 @@ const store = createStore(
 saga.run(rootWatcher);
 
 export default store;
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
