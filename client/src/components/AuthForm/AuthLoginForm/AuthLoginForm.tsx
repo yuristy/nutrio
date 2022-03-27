9c8041b1b7ec12c78b73d481e78d5c711e88ffe5
Yuri Strelchenya <yoosambee@gmail.com>
2022-03-27 22:33:47 +0300

    Add form components

---
+++ client/src/components/AuthForm/AuthLoginForm/AuthLoginForm.tsx
@@ -0,0 +1,107 @@
+import React, { useState, FC, useCallback, useEffect } from 'react';
+import { useDispatch, connect, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { TextInput, PasswordInput, Button } from '@mantine/core';
+import { At, Key, Lock } from 'tabler-icons-react';
+
+import { useAppSelector, useAppDispatch } from '../../../hooks/hooks';
+import { doLogin } from '../../../store/sagas/authSaga/actions';
+
+import { IAuthInfo, IUserAuthInfo } from '../../../interfaces';
+
+const AuthLoginForm: FC = () => {
+    const dispatcher = useDispatch();
+    const isAuthenticated = useSelector(
+        (state: IUserAuthInfo) => state.authInfo.isAuthenticated
+    );
+
+    const isLoginLoading = useSelector((state: any) => state.uiInfo.isLoginLoading);
+    let navigate = useNavigate();
+
+    useEffect(() => {
+        if (isAuthenticated) {
+            return navigate('/');
+        }
+    }, [isAuthenticated]);
+
+    const [formValue, setFormValue] = useState<IAuthInfo>({
+        email: '',
+        password: '',
+    });
+
+    const handleSubmit = useCallback(
+        async (e: React.FormEvent<HTMLFormElement>) => {
+            e.preventDefault();
+            dispatcher(doLogin(formValue));
+        },
+        [dispatcher, formValue]
+    );
+
+    const handleChange = useCallback(
+        (e: React.ChangeEvent<HTMLInputElement>) => {
+            const { name, value } = e.target;
+            setFormValue((prevState) => {
+                return {
+                    ...prevState,
+                    [name]: value,
+                };
+            });
+        },
+        []
+    );
+
+    return (
+        <div className="wrapper">
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <TextInput
+                        icon={<At />}
+                        placeholder="Ваша почта"
+                        label="Ваша почта"
+                        className="form-control"
+                        onChange={handleChange}
+                        id="email"
+                        name="email"
+                        type="email"
+                        required
+                        value={formValue.email}
+                        aria-describedby="emailHelp"
+                        mb={10}
+                    />
+                </div>
+                <div>
+                    <PasswordInput
+                        icon={<Lock />}
+                        placeholder="Пароль"
+                        label="Пароль"
+                        className="form-control"
+                        onChange={handleChange}
+                        id="password"
+                        name="password"
+                        required
+                        autoComplete="on"
+                        value={formValue.password}
+                        mb={10}
+                    />
+                </div>
+                <Button
+                    // leftIcon={<Key size={18} />}
+                    loading={isLoginLoading}
+                    type="submit"
+                    variant="gradient"
+                    gradient={{ from: 'violet', to: 'grape', deg: 105 }}
+                >
+                    Войти
+                </Button>
+            </form>
+        </div>
+    );
+};
+
+// function mapStateToProps(state) {
+// const {isAuthenticated} = state;
+// return { isAuthenticated: true }
+// }
+
+export default AuthLoginForm;
+// export default connect(mapStateToProps)(AuthLoginForm)
---
+++ client/src/components/AuthForm/AuthRegForm/AuthRegForm.tsx
@@ -0,0 +1,82 @@
+import React, { useState, FC } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { TextInput, PasswordInput, Button } from '@mantine/core';
+import { At, Lock, Check } from 'tabler-icons-react';
+import { doRegistration } from '../../../store/sagas/authSaga/actions';
+
+import { IAuthInfo } from '../../../interfaces';
+
+const AuthRegForm: FC = () => {
+    const dispatcher = useDispatch();
+
+    const [formValue, setFormValue] = useState<IAuthInfo>({
+        email: '',
+        password: '',
+    });
+
+    const isRegLoading = useSelector((state: any) => state.uiInfo.isRegLoading);
+
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        dispatcher(doRegistration(formValue));
+    };
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.target;
+        setFormValue((prevState) => {
+            return {
+                ...prevState,
+                [name]: value,
+            };
+        });
+    };
+
+    return (
+        <div className="wrapper">
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <TextInput
+                        icon={<At />}
+                        placeholder="Ваша почта"
+                        label="Ваша почта"
+                        description="Мы ни с кем не поделимся Вашей почтой."
+                        className="form-control"
+                        onChange={handleChange}
+                        id="email"
+                        name="email"
+                        type="email"
+                        required
+                        value={formValue.email}
+                        mb={10}
+                    />
+                </div>
+                <div>
+                    <PasswordInput
+                        icon={<Lock />}
+                        placeholder="Пароль"
+                        label="Пароль"
+                        description="Должен быть больше 4 и меньше 10 символов"
+                        className="form-control"
+                        onChange={handleChange}
+                        id="password"
+                        name="password"
+                        required
+                        value={formValue.password}
+                        mb={10}
+                    />
+                </div>
+                <Button
+                    leftIcon={<Check size={18} />}
+                    loading={isRegLoading}
+                    type="submit"
+                    variant="gradient"
+                    gradient={{ from: 'violet', to: 'grape', deg: 105 }}
+                >
+                    Регистрация
+                </Button>
+            </form>
+        </div>
+    );
+};
+
+export default AuthRegForm;
