cafd0cccb98492c36ced601ec3093cdf473c3138
Yuri Strelchenya <yoosambee@gmail.com>
2022-03-27 22:36:08 +0300

    Add app routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,22 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect, useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
 import './App.scss';
+import { setUserInfo } from './store/reducers/authReducer/authActions';
 
-import AuthForm from './components/AuthForm/AuthForm';
+import AuthRegForm from './components/AuthForm/AuthRegForm/AuthRegForm';
+import AuthLoginForm from './components/AuthForm/AuthLoginForm/AuthLoginForm';
+import Welcome from './components/Welcome/Welcome';
 
 const App: FC = () => {
     return (
-        <div className="wrapper">
-            <AuthForm />
-        </div>
+        <Router>
+            <Routes>
+                <Route path="/" element={<Welcome />} />
+                <Route path="/registration" element={<AuthRegForm />} />
+                <Route path="/login" element={<AuthLoginForm />} />
+            </Routes>
+        </Router>
     );
 };
 
