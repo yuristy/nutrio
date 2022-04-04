5bb26a272f18780c0eb82565172c4fe078040c10
Yuri Strelchenya <yoosambee@gmail.com>
2022-04-04 08:41:32 +0300

    Add response interceptor

--- client/src/http/index.ts
+++ client/src/http/index.ts
@@ -1,5 +1,6 @@
-import axios, { AxiosRequestConfig, AxiosInstance } from 'axios';
+import axios, { AxiosRequestConfig, AxiosInstance, AxiosResponse } from 'axios';
 import { API_URL, token } from '../variables.js';
+import { IAuthResponse } from '../interfaces/index.js';
 
 const $api: AxiosInstance = axios.create({
     withCredentials: true,
@@ -12,6 +13,24 @@ $api.interceptors.request.use((config: AxiosRequestConfig) => {
         Authorization: `Bearer ${token}`,
     };
     return config;
+});
+
+$api.interceptors.response.use((config: AxiosResponse) => {
+    return config;
+}, async (error) => {
+    try {
+        const originalRequest = error.config;
+        if (error.response.status === 401 && error.config && !error.config._isRetry) {
+            originalRequest._isRetry = true;
+            const response: AxiosResponse<IAuthResponse> = await axios.get(`${API_URL}/auth/refresh`, { withCredentials: true });
+            localStorage.setItem('token', response.data.accessToken)
+            return $api.request(originalRequest);
+        }
+    }
+    catch (e) {
+        console.log('НЕ АВТОРИЗОВАН');
+    }
+    throw error;
 });
 
 export default $api;
