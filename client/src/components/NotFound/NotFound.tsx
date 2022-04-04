f92a9bf1755e7cdb0ddc832ad1b47be029057135
Yuri Strelchenya <yoosambee@gmail.com>
2022-04-04 22:16:37 +0300

    Add Not Found component

---
+++ client/src/components/NotFound/NotFound.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './NotFound.scss'
+
+export const NotFound = () => {
+    return (
+        <div className='wrapper__not_found'>
+            <h2>Ой! 404</h2>
+            <p>Такой страницы не найдено</p>
+        </div>
+    );
+};
