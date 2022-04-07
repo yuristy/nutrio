ad881fa84e7865e35e6b41ef9209b258834b6e89
Yuri Strelchenya <yoosambee@gmail.com>
2022-04-07 11:01:58 +0300

    Rewrite redux to redux/toolkit

--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -1,18 +1,21 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+import { configureStore } from '@reduxjs/toolkit';
 import createSagaMiddleware from 'redux-saga';
+import logger from 'redux-logger';
 import { rootWatcher } from './sagas';
 import rootReducer from './reducers';
-import { composeWithDevTools } from 'redux-devtools-extension';
 
+const devMode = process.env.REACT_APP_NODE_ENV === 'development';
 const saga = createSagaMiddleware();
 
-const store = createStore(
-    rootReducer,
-    compose(applyMiddleware(saga), composeWithDevTools())
-);
+const store = configureStore({
+    reducer: rootReducer,
+    devTools: devMode,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware().concat(logger, saga),
+});
 
 saga.run(rootWatcher);
 
 export default store;
-export type RootState = ReturnType<typeof store.getState>
-export type AppDispatch = typeof store.dispatch
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
--- client/src/store/reducers/index.ts
+++ client/src/store/reducers/index.ts
@@ -1,7 +1,10 @@
-import { combineReducers } from 'redux';
+import { combineReducers } from '@reduxjs/toolkit';
 import authReducer from './authReducer/authReducer';
 import uiReducer from './uiReducer/uiReducer';
 
-const rootReducer = combineReducers({ authInfo: authReducer, uiInfo: uiReducer });
+const rootReducer = combineReducers({
+    authInfo: authReducer,
+    uiInfo: uiReducer,
+});
 
 export default rootReducer;
