9e6b52e67c40cf35d7e5f7fc1ed8817d9043f89e
Yuri Strelchenya <yoosambee@gmail.com>
2022-04-10 23:27:14 +0300

    Rewrite forms with react-hook-form

--- client/src/components/AuthForm/AuthLoginForm/AuthLoginForm.tsx
+++ client/src/components/AuthForm/AuthLoginForm/AuthLoginForm.tsx
@@ -1,5 +1,6 @@
-import React, { useState, FC, useCallback, useEffect } from 'react';
+import { FC, useCallback, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useForm, SubmitHandler } from 'react-hook-form';
 import { TextInput, PasswordInput, Button } from '@mantine/core';
 import { At, Lock } from 'tabler-icons-react';
 import { useAppDispatch, useAppSelector } from '../../../hooks';
@@ -7,93 +8,69 @@ import { doLogin } from '../../../store/sagas/authSaga/actions';
 import { IAuthInfo } from '../../../interfaces';
 
 const AuthLoginForm: FC = () => {
-    const dispatcher = useAppDispatch();
-    const isAuthenticated = useAppSelector(
-        (state) => state.authInfo?.isAuthenticated
-    );
+  const dispatcher = useAppDispatch();
 
-    const { isLoginLoading } = useAppSelector((state) => state.uiInfo);
+  const isAuthenticated = useAppSelector(
+    (state) => state.authInfo?.isAuthenticated
+  );
+  const { isLoginLoading } = useAppSelector((state) => state.uiInfo);
 
-    let navigate = useNavigate();
+  let navigate = useNavigate();
 
-    useEffect(() => {
-        if (isAuthenticated) {
-            return navigate('/');
-        }
-    });
+  useEffect(() => {
+    if (isAuthenticated) {
+      return navigate('/');
+    }
+  });
 
-    const [formValue, setFormValue] = useState<IAuthInfo>({
-        email: '',
-        password: '',
-    });
+  const { register, handleSubmit } = useForm<IAuthInfo>();
 
-    const handleSubmit = useCallback(
-        async (e: React.FormEvent<HTMLFormElement>) => {
-            e.preventDefault();
-            dispatcher(doLogin(formValue));
-        },
-        [dispatcher, formValue]
-    );
+  const onSubmit: SubmitHandler<IAuthInfo> = useCallback(
+    (data) => dispatcher(doLogin(data)),
+    [dispatcher]
+  );
 
-    const handleChange = useCallback(
-        (e: React.ChangeEvent<HTMLInputElement>) => {
-            const { name, value } = e.target;
-            setFormValue((prevState) => {
-                return {
-                    ...prevState,
-                    [name]: value,
-                };
-            });
-        },
-        []
-    );
-
-    return (
-        <div className="wrapper">
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <TextInput
-                        icon={<At />}
-                        placeholder="Ваша почта"
-                        label="Ваша почта"
-                        className="form-control"
-                        onChange={handleChange}
-                        id="email"
-                        name="email"
-                        type="email"
-                        required
-                        value={formValue.email}
-                        aria-describedby="emailHelp"
-                        mb={10}
-                    />
-                </div>
-                <div>
-                    <PasswordInput
-                        icon={<Lock />}
-                        placeholder="Пароль"
-                        label="Пароль"
-                        className="form-control"
-                        onChange={handleChange}
-                        id="password"
-                        name="password"
-                        required
-                        autoComplete="on"
-                        value={formValue.password}
-                        mb={10}
-                    />
-                </div>
-                <Button
-                    // leftIcon={<Key size={18} />}
-                    loading={isLoginLoading}
-                    type="submit"
-                    variant="gradient"
-                    gradient={{ from: 'violet', to: 'grape', deg: 105 }}
-                >
-                    Войти
-                </Button>
-            </form>
+  return (
+    <div className="wrapper">
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div>
+          <TextInput
+            icon={<At />}
+            placeholder="Ваша почта"
+            label="Ваша почта"
+            className="form-control"
+            id="email"
+            type="email"
+            required
+            aria-describedby="emailHelp"
+            mb={10}
+            {...register('email')}
+          />
         </div>
-    );
+        <div>
+          <PasswordInput
+            icon={<Lock />}
+            placeholder="Пароль"
+            label="Пароль"
+            className="form-control"
+            id="password"
+            required
+            autoComplete="on"
+            mb={10}
+            {...register('password')}
+          />
+        </div>
+        <Button
+          loading={isLoginLoading}
+          type="submit"
+          variant="gradient"
+          gradient={{ from: 'violet', to: 'grape', deg: 105 }}
+        >
+          Войти
+        </Button>
+      </form>
+    </div>
+  );
 };
 
 export default AuthLoginForm;
--- client/src/components/AuthForm/AuthRegForm/AuthRegForm.tsx
+++ client/src/components/AuthForm/AuthRegForm/AuthRegForm.tsx
@@ -1,94 +1,77 @@
-import React, { useState, useEffect, FC } from 'react';
+import { useEffect, useCallback, FC } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useForm, SubmitHandler } from 'react-hook-form';
 import { TextInput, PasswordInput, Button } from '@mantine/core';
-import { useAppDispatch, useAppSelector } from '../../../hooks';
 import { At, Lock, Check } from 'tabler-icons-react';
+import { useAppDispatch, useAppSelector } from '../../../hooks';
 import { doRegistration } from '../../../store/sagas/authSaga/actions';
 
 import { IAuthInfo } from '../../../interfaces';
 
 const AuthRegForm: FC = () => {
-    const dispatcher = useAppDispatch();
-    const isAuthenticated = useAppSelector(
-        (state) => state.authInfo?.isAuthenticated
-    );
+  const dispatcher = useAppDispatch();
+  const isAuthenticated = useAppSelector(
+    (state) => state.authInfo?.isAuthenticated
+  );
 
-    const [formValue, setFormValue] = useState<IAuthInfo>({
-        email: '',
-        password: '',
-    });
+  const { register, handleSubmit } = useForm<IAuthInfo>();
+  const { isRegLoading } = useAppSelector((state) => state.uiInfo);
 
-    const { isRegLoading } = useAppSelector((state) => state.uiInfo);
+  let navigate = useNavigate();
 
-    let navigate = useNavigate();
+  useEffect(() => {
+    if (isAuthenticated) {
+      return navigate('/');
+    }
+  });
 
-    useEffect(() => {
-        if (isAuthenticated) {
-            return navigate('/');
-        }
-    });
+  const onSubmit: SubmitHandler<IAuthInfo> = useCallback(
+    (data) => dispatcher(doRegistration(data)),
+    [dispatcher]
+  );
 
-    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        dispatcher(doRegistration(formValue));
-    };
-
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const { name, value } = e.target;
-        setFormValue((prevState) => {
-            return {
-                ...prevState,
-                [name]: value,
-            };
-        });
-    };
-
-    return (
-        <div className="wrapper">
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <TextInput
-                        icon={<At />}
-                        placeholder="Ваша почта"
-                        label="Ваша почта"
-                        description="Мы ни с кем не поделимся Вашей почтой."
-                        className="form-control"
-                        onChange={handleChange}
-                        id="email"
-                        name="email"
-                        type="email"
-                        required
-                        value={formValue.email}
-                        mb={10}
-                    />
-                </div>
-                <div>
-                    <PasswordInput
-                        icon={<Lock />}
-                        placeholder="Пароль"
-                        label="Пароль"
-                        description="Должен быть больше 4 и меньше 10 символов"
-                        className="form-control"
-                        onChange={handleChange}
-                        id="password"
-                        name="password"
-                        required
-                        value={formValue.password}
-                        mb={10}
-                    />
-                </div>
-                <Button
-                    leftIcon={<Check size={18} />}
-                    loading={isRegLoading}
-                    type="submit"
-                    variant="gradient"
-                    gradient={{ from: 'violet', to: 'grape', deg: 105 }}
-                >
-                    Регистрация
-                </Button>
-            </form>
+  return (
+    <div className="wrapper">
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div>
+          <TextInput
+            icon={<At />}
+            placeholder="Ваша почта"
+            label="Ваша почта"
+            description="Мы ни с кем не поделимся Вашей почтой."
+            className="form-control"
+            id="email"
+            type="email"
+            required
+            mb={10}
+            {...register('email')}
+          />
         </div>
-    );
+        <div>
+          <PasswordInput
+            icon={<Lock />}
+            placeholder="Пароль"
+            label="Пароль"
+            description="Должен быть больше 4 и меньше 10 символов"
+            className="form-control"
+            id="password"
+            required
+            mb={10}
+            {...register('password')}
+          />
+        </div>
+        <Button
+          leftIcon={<Check size={18} />}
+          loading={isRegLoading}
+          type="submit"
+          variant="gradient"
+          gradient={{ from: 'violet', to: 'grape', deg: 105 }}
+        >
+          Регистрация
+        </Button>
+      </form>
+    </div>
+  );
 };
 
 export default AuthRegForm;
